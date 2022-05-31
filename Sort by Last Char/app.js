5553365c3f552d2d874c34285f2d3567b8ab8c1e
JulianSono <54417343+JulianSono@users.noreply.github.com>
2022-05-31 13:34:58 +0100

    Sort by Last Char

---
+++ Sort by Last Char/app.js
@@ -0,0 +1,23 @@
+/*
+
+Sort by Last Char:
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+
+
+*/
+
+function last(x){
+
+let res = x.split(" ")
+
+
+    return res.sort((a,b)=> a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1) )
+}
+
+last('man i need a taxi up to ubud')
