12646b4a50f506cc7203d1df2f7ff923c847827c
JulianSono <54417343+JulianSono@users.noreply.github.com>
2022-09-12 14:08:02 +0100

    Find the unique string

---
+++ Find the unique string/app.js
@@ -0,0 +1,15 @@
+/*
+There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+Strings may contain spaces. Spaces are not significant, only non-spaces symbols matters. E.g. string that contains only spaces is like empty string.
+
+It’s guaranteed that array contains more than 2 strings.
+*/
+
+function findUniq(arr) {
+   return arr.sort().filter((e, i, array) => e.match(new RegExp(`[^${array[1]}]`, 'gi')));
+ }
+
+findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])
